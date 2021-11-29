a4e5b1cf8e1503b626b00963161cb2ee6e5a39b5
Patryk Blaszczak <patrykblaszczak1@gmail.ocm>
2021-11-29 09:55:14 +0000

    rewrite some logic + refactor code

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ let speedButtons = document.querySelectorAll(".speed-button")
 let mainGame_window = document.querySelector("#main-game-window");
 let scoreTable = document.querySelector("#score-table");
 let score = 0;
-let scoreCounter = 0;
+let foodCounter = 0;
+let foodTimer = 5;
 let exitGame_button = document.querySelector("#exit-game-button");
 let finalScore = document.querySelector("#final-score")
 let gameOverScreen = document.querySelector("#game-over-screen")
 let direction = "left";
 let savedPosition = { row: 0, column: 0 };
-let copyOfCurrentPosition = { row: 0, column: 0 }
 let snakeLength = 4;
 let gameSpeed = 120;
 let mainInterval = undefined;
-let foodCounter = 5;
-let eatCounter = "";
+let eatTimer = undefined;
+let foodPosition = { row: undefined, column: undefined };
 
 let mainGameboardArray = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -65,15 +65,15 @@ let previewGameboardArray = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-let mainSnake = [{ part: "head", position: { row: 18, column: 30 } }, { part: "body", position: { row: 18, column: 31 } }, { part: "body", position: { row: 18, column: 32 } }, { part: "body", position: { row: 18, column: 33 } }, { part: "body", position: { row: 18, column: 34 } }];
-let previewSnake = [{ part: "head", position: { row: 3, column: 10 } }, { part: "body", position: { row: 3, column: 9 } }, { part: "body", position: { row: 3, column: 8 } }, { part: "body", position: { row: 3, column: 7 } }, { part: "body", position: { row: 3, column: 6 } }];
+let mainSnakeData = [{ part: "head", position: { row: 18, column: 30 } }, { part: "body", position: { row: 18, column: 31 } }, { part: "body", position: { row: 18, column: 32 } }, { part: "body", position: { row: 18, column: 33 } }, { part: "body", position: { row: 18, column: 34 } }];
+let previewSnakeData = [{ part: "head", position: { row: 3, column: 10 } }, { part: "body", position: { row: 3, column: 9 } }, { part: "body", position: { row: 3, column: 8 } }, { part: "body", position: { row: 3, column: 7 } }, { part: "body", position: { row: 3, column: 6 } }];
 
 startGame_button.addEventListener("click", () => {
-    clearInterval(startGameWindowInterval);
     direction = "left";
+    clearInterval(startGameWindowInterval);
     document.addEventListener("mousedown", enableTouchControl);
-    mainInterval = setInterval(game, gameSpeed);
     document.addEventListener("keydown", enableKeys)
+    mainInterval = setInterval(gameLoop, gameSpeed);
     setTimeout(() => {
         clearInterval(startGameWindowInterval)
         startGame_window.style.display = "none";
@@ -82,27 +82,57 @@ startGame_button.addEventListener("click", () => {
 })
 
 exitGame_button.addEventListener("click", () => {
+    document.removeEventListener("mousedown", enableTouchControl);
+    document.removeEventListener("keydown", enableKeys)
     score = 0;
     scoreTable.textContent = "0";
     direction = "left";
-    document.removeEventListener("mousedown", enableTouchControl);
     clearInterval(mainInterval);
-    disableKeys()
     gameSpeed = 120;
-    startGameWindowInterval = setInterval(preview, gameSpeed)
+    startGameWindowInterval = setInterval(previewLoop, gameSpeed)
     mainGame_window.style.display = "none";
     gameOverScreen.style.display = "none";
     startGame_window.style.display = "flex";
-    resetGameboard(mainGameboardArray)
+    resetGameboardArray(mainGameboardArray)
     resetSnake();
 })
 
-programSpeedButtons(enableSpeedButtons)
+toggleSpeedButtons(enableSpeedButtons)
 renderGameboard(previewGameboard, previewGameboardArray);
+let startGameWindowInterval = setInterval(previewLoop, gameSpeed)
+
+function gameLoop() {
+    if (eatTimer === 0) {
+        deleteFood();
+    }
+    if (foodTimer === 0) {
+        putFoodInGameboardArray();
+        eatTimer = 55;
+        foodTimer = 60;
+    }
+    foodTimer--;
+    eatTimer--;
+
+    mainGameboard.innerHTML = "";
+    updateSnakePosition(mainSnakeData, mainGameboardArray);
+    if (foundFood()) {
+        foodTimer = 0;
+        extendSnake();
+    }
+
+    if (collision()) {
+        endGame();
+    }
+
+    resetGameboardArray(mainGameboardArray);
+    updateGameboardArray(mainGameboardArray, mainSnakeData);
+    renderGameboard(mainGameboard, mainGameboardArray);
+
+
+}
 
 function enableTouchControl(event) {
     if (direction === "up" || direction === "down") {
-        console.log(getSnakePositionOnScreen().x)
         if (getSnakePositionOnScreen().x < event.clientX) {
             direction = "right";
         } else if (getSnakePositionOnScreen().x > event.clientX) {
@@ -117,137 +147,124 @@ function enableTouchControl(event) {
     }
 }
 
-
-let startGameWindowInterval = setInterval(preview, gameSpeed)
-
 function getSnakePositionOnScreen() {
     let allGameBoardFields = document.querySelectorAll(".field")
-    let positon = "";
-    allGameBoardFields.forEach(field => {
-        if (field.classList.contains("snake-head")) {
-            let fieldPositionProperties = field.getBoundingClientRect();
-            let x = Math.round(fieldPositionProperties.x + (fieldPositionProperties.width/2));
-            let y = Math.round(fieldPositionProperties.y + (fieldPositionProperties.height/2))
-            positon = {x: x, y: y}
+    for (let i = 0; i < allGameBoardFields.length; i++) {
+        if (allGameBoardFields[i].classList.contains("snake-head")) {
+            let fieldPositionProperties = allGameBoardFields[i].getBoundingClientRect();
+            let x = Math.round(fieldPositionProperties.x + (fieldPositionProperties.width / 2));
+            let y = Math.round(fieldPositionProperties.y + (fieldPositionProperties.height / 2))
+            return { x: x, y: y }
         }
-    })
-    return positon;
+    }
 }
 
-function game() {
-    if (eatCounter === 0) {
-        deleteFood();
+function foundFood() {
+    if (mainSnakeData[0].position.row === foodPosition.row && mainSnakeData[0].position.column === foodPosition.column) {
+        foodCounter++;
+        return true;
     }
-    if (foodCounter === 0) {
-        generateFood();
-        eatCounter = 55;
-        foodCounter = 60;
-    }
-    foodCounter--;
-    eatCounter--;
-
-    mainGameboard.innerHTML = "";
-    moveSnake(mainSnake, mainGameboardArray);
-    placeSnakeOnBoard(mainGameboardArray, mainSnake);
-    resetGameboard(mainGameboardArray);
-    placeSnakeOnBoard(mainGameboardArray, mainSnake);
-    renderGameboard(mainGameboard, mainGameboardArray);
 }
 
-function preview() {
+function collision() {
+    if (mainGameboardArray[mainSnakeData[0].position.row][mainSnakeData[0].position.column] === 2) {
+        return true;
+    }
+}
+
+function endGame() {
+    document.removeEventListener("mousedown", enableTouchControl);
+    document.removeEventListener("keydown", enableKeys)
+    clearInterval(mainInterval);
+    finalScore.textContent = score;
+    gameOverScreen.style.display = "flex";
+    finalScore.textContent = score;
+}
+
+function extendSnake() {
+    mainSnakeData.push({ part: "body", position: { row: savedPosition.row, column: savedPosition.column } })
+    score += 10;
+    scoreTable.textContent = score;
+    if (foodCounter === 6 && gameSpeed > 10) {
+        foodCounter = 0;
+        gameSpeed -= 10;
+        clearInterval(mainInterval);
+        mainInterval = mainInterval = setInterval(gameLoop, gameSpeed);
+    }
+}
+
+function previewLoop() {
     previewGameboard.innerHTML = "";
-    resetGameboard(previewGameboardArray);
-    moveSnake(previewSnake, previewGameboardArray);
-    placeSnakeOnBoard(previewGameboardArray, previewSnake);
+    resetGameboardArray(previewGameboardArray);
+    updateSnakePosition(previewSnakeData, previewGameboardArray);
+    updateGameboardArray(previewGameboardArray, previewSnakeData);
     renderGameboard(previewGameboard, previewGameboardArray);
 }
 
 function resetSnake() {
-    let counter = mainSnake.length - 1;
+    let counter = mainSnakeData.length - 1;
     let snakeDefaultState = [{ part: "head", position: { row: 18, column: 30 } }, { part: "body", position: { row: 18, column: 31 } }, { part: "body", position: { row: 18, column: 32 } }, { part: "body", position: { row: 18, column: 33 } }, { part: "body", position: { row: 18, column: 34 } }];
     for (let i = 0; i < counter; i++) {
         if (i >= snakeDefaultState.length - 1) {
-            mainSnake.pop();
+            mainSnakeData.pop();
         } else {
-            mainSnake[i].position.row = snakeDefaultState[i].position.row;
-            mainSnake[i].position.column = snakeDefaultState[i].position.column;
+            mainSnakeData[i].position.row = snakeDefaultState[i].position.row;
+            mainSnakeData[i].position.column = snakeDefaultState[i].position.column;
         }
     }
 }
 
-function moveSnake(snake, gameboardArray) {
-    for (let i = 0; i < snake.length; i++) {
-        if (snake[i].part === "head") {
-            savePosition(snake[i].position.row, snake[i].position.column)
-            moveSnakeHead(i, gameboardArray, snake)
-        } else if (snake[i].part === "body") {
-            saveCopyOfCurrentPosition(snake[i].position.row, snake[i].position.column)
-            moveSnakeBody(i, snake);
-            savePosition(copyOfCurrentPosition.row, copyOfCurrentPosition.column)
+function updateSnakePosition(snake, gameboardArray) {
+    moveSnakeBody(snake)
+    moveSnakeHead(gameboardArray, snake);
+}
+
+function moveSnakeBody(snake) {
+    for (let i = snake.length - 1; i > 0; i--) {
+        if (i === snake.length - 1) {
+            savedPosition = {row:snake[i].position.row, column:snake[i].position.column}
         }
+        snake[i].position.row = snake[i - 1].position.row;
+        snake[i].position.column = snake[i - 1].position.column;
     }
 }
 
-function moveSnakeHead(index, gameboardArray, snake) {
+function moveSnakeHead(gameboardArray, snake) {
     switch (direction) {
         case "up":
-            if (snake[index].position.row === 0) {
-                snake[index].position.row = gameboardArray.length - 1;
+            if (snake[0].position.row === 0) {
+                snake[0].position.row = gameboardArray.length - 1;
                 break;
             }
-            snake[index].position.row = snake[index].position.row - 1;
+            snake[0].position.row = snake[0].position.row - 1;
             break;
         case "down":
-            if (snake[index].position.row === gameboardArray.length - 1) {
-                snake[index].position.row = 0;
+            if (snake[0].position.row === gameboardArray.length - 1) {
+                snake[0].position.row = 0;
                 break;
             }
-            snake[index].position.row = snake[index].position.row + 1;
+            snake[0].position.row = snake[0].position.row + 1;
             break;
         case "left":
-            if (snake[index].position.column === 0) {
-                snake[index].position.column = gameboardArray[0].length - 1;
+            if (snake[0].position.column === 0) {
+                snake[0].position.column = gameboardArray[0].length - 1;
                 break;
             }
-            snake[index].position.column = snake[index].position.column - 1;
+            snake[0].position.column = snake[0].position.column - 1;
             break;
         case "right":
-            if (snake[index].position.column === gameboardArray[0].length - 1) {
-                snake[index].position.column = 0;
+            if (snake[0].position.column === gameboardArray[0].length - 1) {
+                snake[0].position.column = 0;
                 break;
             }
-            snake[index].position.column = snake[index].position.column + 1;
+            snake[0].position.column = snake[0].position.column + 1;
             break;
     }
 }
 
-function moveSnakeBody(index, snake) {
-    snake[index].position.row = savedPosition.row;
-    snake[index].position.column = savedPosition.column;
-}
-
-function placeSnakeOnBoard(gameboardArray, snake) {
+function updateGameboardArray(gameboardArray, snake) {
     snake.forEach(element => {
         if (element.part === "head") {
-            if (gameboardArray[element.position.row][element.position.column] === 2) {
-                clearInterval(mainInterval);
-                finalScore.textContent = score;
-                gameOverScreen.style.display = "flex";
-                finalScore.textContent = score;
-            }
-            if (gameboardArray[element.position.row][element.position.column] === 3) {
-                snake.push({ part: "body", position: { row: savedPosition.row, column: savedPosition.column } })
-                score += 10;
-                scoreTable.textContent = score;
-                scoreCounter++;
-                foodCounter = 0;
-                if (scoreCounter === 6 && gameSpeed > 10) {
-                    scoreCounter = 0;
-                    gameSpeed -= 10;
-                    clearInterval(mainInterval);
-                    mainInterval = mainInterval = setInterval(game, gameSpeed);
-                }
-            }
             gameboardArray[element.position.row][element.position.column] = 1;
         } else if (element.part === "body") {
             gameboardArray[element.position.row][element.position.column] = 2;
@@ -260,27 +277,27 @@ function enableSpeedButtons(button) {
         case "speed1":
             gameSpeed = 250;
             clearInterval(startGameWindowInterval)
-            startGameWindowInterval = setInterval(preview, gameSpeed)
+            startGameWindowInterval = setInterval(previewLoop, gameSpeed)
             break;
         case "speed2":
             gameSpeed = 180;
             clearInterval(startGameWindowInterval)
-            startGameWindowInterval = setInterval(preview, gameSpeed)
+            startGameWindowInterval = setInterval(previewLoop, gameSpeed)
             break;
         case "speed3":
             gameSpeed = 120;
             clearInterval(startGameWindowInterval)
-            startGameWindowInterval = setInterval(preview, gameSpeed)
+            startGameWindowInterval = setInterval(previewLoop, gameSpeed)
             break;
         case "speed4":
             gameSpeed = 80;
             clearInterval(startGameWindowInterval)
-            startGameWindowInterval = setInterval(preview, gameSpeed)
+            startGameWindowInterval = setInterval(previewLoop, gameSpeed)
             break;
         case "speed5":
             gameSpeed = 50;
             clearInterval(startGameWindowInterval)
-            startGameWindowInterval = setInterval(preview, gameSpeed)
+            startGameWindowInterval = setInterval(previewLoop, gameSpeed)
             break;
     }
 }
@@ -291,7 +308,7 @@ function disableSpeedButtons() {
     })
 }
 
-function programSpeedButtons(handler) {
+function toggleSpeedButtons(handler) {
     speedButtons.forEach(button => {
         button.addEventListener("click", handler)
     })
@@ -325,7 +342,6 @@ function enableKeys(event) {
 function renderGameboard(gameboard, gameboardArray) {
     for (let row = 0; row < gameboardArray.length; row++) {
         let newRow = createRow();
-
         for (let column = 0; column < gameboardArray[row].length; column++) {
             if (gameboardArray[row][column] === 0) {
                 newRow.appendChild(createField("empty-field", gameboard));
@@ -341,7 +357,7 @@ function renderGameboard(gameboard, gameboardArray) {
     }
 }
 
-function resetGameboard(gameboardArray) {
+function resetGameboardArray(gameboardArray) {
     for (let row = 0; row < gameboardArray.length; row++) {
         for (let column = 0; column < gameboardArray[row].length; column++) {
             if (gameboardArray[row][column] !== 3)
@@ -350,12 +366,13 @@ function resetGameboard(gameboardArray) {
     }
 }
 
-function generateFood() {
+function putFoodInGameboardArray() {
     while (true) {
         let row = Math.round(Math.random() * (mainGameboardArray.length - 1));
         let column = Math.round(Math.random() * (mainGameboardArray[0].length - 1));
         if (mainGameboardArray[row][column] === 0) {
             mainGameboardArray[row][column] = 3;
+            foodPosition = { row: row, column: column }
             break;
         }
     }
@@ -393,12 +410,7 @@ function disableKeys() {
     document.removeEventListener("keydown", enableKeys)
 }
 
-function savePosition(row, column) {
-    savedPosition.row = row;
-    savedPosition.column = column;
-}
-
-function saveCopyOfCurrentPosition(row, column) {
-    copyOfCurrentPosition.row = row;
-    copyOfCurrentPosition.column = column;
+function saveCurrentBodyPartPosition(row, column) {
+    currentBodyPartPosition.row = row;
+    currentBodyPartPosition.column = column;
 }
